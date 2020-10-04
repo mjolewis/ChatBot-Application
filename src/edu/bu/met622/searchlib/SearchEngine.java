c2e2a3ebdd59ad08fe71935091652e14a54b285c
Michael Lewis <mjolewis@gmail.com>
2020-10-04 13:23:02 -0400

    Style: Improved readability of search results

--- src/edu/bu/met622/searchlib/SearchEngine.java
+++ src/edu/bu/met622/searchlib/SearchEngine.java
@@ -7,8 +7,10 @@ import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.document.Document;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -20,6 +22,8 @@ import java.nio.file.Paths;
 public class SearchEngine {
     IndexSearcher searcher = null;
     QueryParser parser = null;
+    TopDocs docs = null;
+    ScoreDoc[] hits = null;
 
 
     /**
@@ -41,15 +45,35 @@ public class SearchEngine {
      *
      * @param searchParam A user specified search string
      * @param numOfDocs   The maximum number of documents to return when the search parameter is found
-     * @return The top N documents that matches the search parameter
      */
-    public TopDocs search(String searchParam, int numOfDocs) {
+    public void search(String searchParam, int numOfDocs) {
         try {
             Query query = parser.parse(searchParam);
-            return searcher.search(query, numOfDocs);
+            docs = searcher.search(query, numOfDocs);
+            hits = docs.scoreDocs;
         } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
-        return null;
+    }
+
+    public void displayHits() {
+        for (int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+
+            try {
+                Document doc = searcher.doc(docId);
+                System.out.print(i + 1 + ") " + "Pub ID: " + doc.get(Constants.PUB_ID) + "\n" +
+                        "\tTitle: " + doc.get(Constants.ARTICLE_TITLE) + "\n" +
+                        "\tPub Year: " + doc.get(Constants.PUBLICATION_YEAR) + "\n" +
+                        "\tPub Month: " + doc.get(Constants.PUBLICATION_MONTH) + "\n" +
+                        "\tPub Day: " + doc.get(Constants.PUBLICATION_DAY) + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ScoreDoc[] getHits() {
+        return hits;
     }
 }
