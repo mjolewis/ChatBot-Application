dd240d8699306697d2fa5fa47dedb1f0e8deec33
Michael Lewis <mjolewis@gmail.com>
2020-11-20 16:33:22 -0500

    FEAT: Implemented BotUI
    BotUI provides conversational flow for the ChatBot

--- src/main/java/edu/bu/met622/controller/ResponseController.java
+++ src/main/java/edu/bu/met622/controller/ResponseController.java
@@ -1,14 +1,22 @@
 package edu.bu.met622.controller;
 
 import edu.bu.met622.Builder;
+import edu.bu.met622.database.LuceneIndex;
+import edu.bu.met622.database.LuceneSearch;
+import edu.bu.met622.database.MongoDB;
 import edu.bu.met622.database.MySQL;
 import edu.bu.met622.model.ClientMessage;
 import edu.bu.met622.model.ServerResponse;
-import edu.bu.met622.utils.XMLParser;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+/**********************************************************************************************************************
+ * A web request handler
+ *
+ * @author Michael Lewis
+ * @version November 20, 2020 - Kickoff
+ *********************************************************************************************************************/
 @Controller
 public class ResponseController {
 
@@ -16,29 +24,46 @@ public class ResponseController {
     String year;
 
     @MessageMapping("/keyword")
-    public void keyword(ClientMessage keyword) throws Exception {
+    public void keyword(ClientMessage keyword) {
         this.keyword = keyword.getKeyword();
     }
 
     @MessageMapping("/year")
-    public void serverResponse(ClientMessage year) throws Exception {
+    public void year(ClientMessage year) {
         this.year = year.getYear();
     }
 
-    @MessageMapping("/search")
+    @MessageMapping("/mysql/search")
     @SendTo("/query/response")
-    public ServerResponse serverResponse() throws Exception {
+    public ServerResponse mySqlResponse() {
         MySQL mySQLDB = MySQL.getInstance();
 
-        int hits = mySQLDB.query(keyword, year);
-        return new ServerResponse(keyword, year, hits);
+        double hits = mySQLDB.query(keyword, year);
+        double runtime = mySQLDB.getRunTime();
+
+        return new ServerResponse(keyword, year, hits, runtime);
     }
 
-    @MessageMapping("/disconnect")
-    public void disconnect() throws Exception {
-        Builder builder = new Builder();
-        builder.cleanup();
-        builder.endMessage();
+    @MessageMapping("/mongodb/search")
+    @SendTo("/query/response")
+    public ServerResponse mongoDBResponse() {
+        MongoDB mongoDB = MongoDB.getInstance();
+
+        double hits = mongoDB.query(keyword, year);
+        double runtime = mongoDB.getRunTime();
+
+        return new ServerResponse(keyword, year, hits, runtime);
     }
 
+    @MessageMapping("/lucene/search")
+    @SendTo("/query/response")
+    public ServerResponse luceneResponse() {
+        LuceneSearch luceneSearch = new LuceneSearch();
+
+        // TODO: 11/20/20 fix number of docs to all docs
+        double hits = luceneSearch.search(keyword, 5000);
+        double runtime = luceneSearch.getRunTime();
+
+        return new ServerResponse(keyword, year, hits, runtime);
+    }
 }
--- target/classes/static/app.js
+++ target/classes/static/app.js
@@ -1,154 +1,159 @@
-var stompClient = null;
-var botui = null;
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
+var socket = new SockJS('/chatbot');
+var stompClient = Stomp.over(socket);
+var botui = new BotUI('botui-app');
+var keyword;
+var year;
 
-function connect() {
-    //botui = new BotUI('botui-app');
-    var socket = new SockJS('/chatbot');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/query/response', function (response) {
-            showResults(JSON.parse(response.body));
-        });
+/**
+ * Establish a websocket connection. Upon successful connection, subscribe to /query/response destination where the
+ * server will publish the query results.
+ */
+stompClient.connect({}, function (frame) {
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/query/response', function (response) {
+        window.result = JSON.parse(response.body);
     });
-}
-
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.send("/app/disconnect", {});
-        stompClient.disconnect();
-    }
-    setConnected(false);
-}
-
-function search() {
-    stompClient.send("/app/search", {}, JSON.stringify({'name': $("#name").val()}));
-}
-
-function sendKeyword() {
-    stompClient.send("/app/keyword", {}, JSON.stringify({'keyword': $("#keyword").val()}));
-}
-
-function sendYear() {
-    stompClient.send("/app/year", {}, JSON.stringify({'year': $("#year").val()}));
-}
-
-function showResults(message) {
-    $("#greetings").append("<tr><td>" + message.keyword + " was found " + message.hits + " times in " + message.year + "</td></tr>");
-}
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#sendKeyword" ).click(function() { sendKeyword(); });
-    $( "#sendYear" ).click(function() { sendYear(); });
-    $( "#search" ).click(function() { search(); });
 });
 
+/**
+ * Send the keyword to endpoint
+ */
+function sendKeyword(keyword) {
+    stompClient.send("/app/keyword", {}, JSON.stringify({'keyword': keyword}));
+}
 
+/**
+ * Send the year to endpoint
+ */
+function sendYear(year) {
+    stompClient.send("/app/year", {}, JSON.stringify({'year': year}));
+}
 
+/**
+ * Search a MySQL database for the keyword
+ */
+function mySqlSearch() {
+    stompClient.send("/app/mysql/search", {});
+}
 
+/**
+ * Search a MongoDB database for the keyword
+ */
+function mongoDBSearch() {
+    stompClient.send("/app/mongodb/search", {});
+}
 
+/**
+ * Search a Lucene Index for the keyword
+ */
+function luceneIndexSearch() {
+    stompClient.send("/app/lucene/search", {});
+}
 
-// var botui = new BotUI('botui-app');                                  // id of container
+/**
+ * A conversation with the user managed by BotUI
+ */
+botui.message.add({                                                  // first message
+    delay: 200,
+    content: 'Welcome to ChatBot'
+}).then(() => {
+    return botui.message.add({                                       // second message
+        delay: 2000,                                                 // wait 1 sec.
+        loading: true,
+        content: "I'll help you search through PubMed! Let's start with this...",
+    })
+}).then(() => {                                                      // Bot continues the conversation
+    return botui.message.add( {
+        delay: 2000,                                                 // wait 1 sec.
+        loading: true,
+        content: "What year should I search in?"
+    })
+}).then(() => {
+    return botui.action.text({
+        action: {
+            placeholder: "Enter a year"
+        }
+    })
+}).then(function(res) {                                                      // Bot continues the conversation
+    year = res.value;
+    sendYear(year);
 
-// botui.message.add({                                                  // first message
-//     delay: 200,
-//     content: 'Welcome to ChatBot'
-// }).then(() => {
-//     return botui.message.add({                                       // second message
-//         delay: 1000,                                                 // wait 1 sec.
-//         content: "I'll help you search through PubMed! Let's start with this...",
-//     })
-// }).then(() => {
-//     return botui.message.add({                                       // third message
-//         delay: 1000,                                                 // wait 1 sec.
-//         content: "What type of search do you want to use?"
-//     })
-// }).then(() => {
-//     return botui.action.select({
-//         action: {
-//             placeholder: "Select the search type",
-//             searchselect: true,                                      // Default: true, false for standard dropdown
-//             label: 'text',                                           // dropdown label variable
-//             options: [
-//                 {value: "BF", text: "BruteForce"},
-//                 {value: "LI", text: "Lucene Index"},
-//                 {value: "SQL", text: "MySQL"},
-//                 {value: "MG", text: "MongoDB"},
-//             ],
-//             button: {
-//                 icon: 'check',
-//                 label: 'OK'
-//             }
-//         }
-//     })
-// }).then(function (res) {                                   // Get the result and continue conversation
-//     searchType = res.value;
-//     return botui.message.add({
-//         delay: 1000,                                                 // wait 1 sec.
-//         content: "Cool! Thanks for the info. Just a few more questions..."
-//     });
-// }).then(() => {                                                      // Bot continues the conversation
-//     return botui.message.add( {
-//         delay: 1000,                                                 // wait 1 sec.
-//         content: "Do you want to do a range query or search within a specific year?"
-//     })
-// }).then(() => {
-//     return time = botui.action.select({
-//         action: {
-//             placeholder: "Your choice...",
-//             searchselect: true,                                      // Default: true, false for standard dropdown
-//             label: 'text',                                           // dropdown label variable
-//             options: [
-//                 {value: "Range", text: "Range of years"},
-//                 {value: "Single", text: "Specific year"},
-//             ],
-//             button: {
-//                 icon: 'check',
-//                 label: 'OK'
-//             }
-//         }
-//     })
-// }).then(() => {                                                      // Bot continues the conversation
-//     return botui.message.add( {
-//         delay: 1000,                                                 // wait 1 sec.
-//         content: "Sounds good! Just one more question...",
-//     })
-// }).then(() => {                                                      // Bot continues the conversation
-//     return botui.message.add( {
-//         delay: 1000,                                                 // wait 1 sec
-//         content: "What do you want to search for? You can enter a keyword like 'Flu'?"
-//     })
-// }).then(() => {
-//     return keyword = botui.action.text({
-//         action: {
-//             placeholder: "Enter a keyword"
-//         }
-//     })
-// }).then(() => {                                                      // Called after user enters a keyword
-//     return botui.message.add( {
-//         delay: 1000,                                                 // wait 1 sec
-//         content: "Ok, I'll search for " + keyword + " in " + time.value + " using " + searchType.value
-//     })
-// }).then(res => {
-//     return botui.message.bot({
-//         delay: 1000,
-//         content: `You are feeling ${res.text}!`
-//     })
-// });
+    return botui.message.add( {
+        delay: 2000,                                                 // wait 1 sec.
+        loading: true,
+        content: "Sounds good! Just one more question...",
+    })
+}).then(() => {                                                      // Bot continues the conversation
+    return botui.message.add( {
+        delay: 2000,                                                 // wait 1 sec
+        loading: true,
+        content: "What do you want to search for? You can enter a keyword like 'Flu'?"
+    })
+}).then(() => {
+    return botui.action.text({
+        action: {
+            placeholder: "Enter a keyword"
+        }
+    })
+}).then(function (res) {                                   // Called after user enters a keyword
+    keyword = res.value;
+    sendKeyword(keyword);
+
+    return botui.message.add( {
+        delay: 2000,                                                 // wait 1 sec
+        loading: true,
+        content: "Ok, I'll search for " + keyword + " in " + year + "..."
+    })
+}).then(() => {
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "For simulation purposes, I am going to search multiple databases so you can compare the runtime " +
+            "performance. I will search a MySQL database, a MongoDB database, a Lucene Index, and using Brute Force..."
+    })
+}).then(() => {
+    mySqlSearch();
+
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "Searching with MySQL...",
+    })
+}).then(() => {
+    return botui.message.add( {
+        delay: 2000,
+        loading: true,
+        content: "It took me " + window.result.runtime + " milliseconds" + " to find the keyword '" + window.result.keyword + "' "
+            + window.result.hits + " times in " + window.result.year
+    })
+}).then(() => {
+    mongoDBSearch();
+
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "Searching with MongoDB...",
+    })
+}).then(() => {
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "It took me " + window.result.runtime + " milliseconds" + " to find the keyword '" + window.result.keyword + "' "
+            + window.result.hits + " times in " + window.result.year
+    })
+}).then(() => {
+    luceneIndexSearch();
+
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "Searching with Lucene Index...",
+    })
+}).then(() => {
+    return botui.message.add({
+        delay: 2000,
+        loading: true,
+        content: "It took me " + window.result.runtime + " milliseconds" + " to find the keyword '" + window.result.keyword + "' "
+            + window.result.hits + " times in " + window.result.year
+    })
+});
