611c18de9ffb1da0e0fba0e0d52407f0313a85b6
Michael Lewis <mjolewis@gmail.com>
2020-11-28 16:50:25 -0500

    FEAT: Added file chooser
    The user can now select the files that will be used to build the knowledge base

--- src/main/java/edu/bu/met622/Builder.java
+++ src/main/java/edu/bu/met622/Builder.java
@@ -7,6 +7,9 @@ import edu.bu.met622.resources.ApplicationConfig;
 import edu.bu.met622.utils.FileMerger;
 import edu.bu.met622.utils.XMLParser;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -37,33 +40,54 @@ public class Builder {
      * selected more than one file to merge
      */
     public void build() {
-        ArrayList<String> selectedFiles = new ArrayList<>();
-        selectedFiles.add(ApplicationConfig.FILE_1);
-        selectedFiles.add(ApplicationConfig.FILE_2);
+        String fileName;
 
-        mergeXML(selectedFiles);
-        initDatabases();
+        JFileChooser jFileChooser = new JFileChooser(FileSystemView.getFileSystemView());
+        jFileChooser.setMultiSelectionEnabled(true);
+
+        File[] selectedFiles;
+        while (true) {
+            int returnValue = jFileChooser.showOpenDialog(null);
+
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                selectedFiles = jFileChooser.getSelectedFiles();
+                if (selectedFiles.length == 1) {                          // Only merge if more than one was selected
+                    initDatabases(selectedFiles[0].getPath());
+                } else {
+                    mergeXML(selectedFiles);
+                    initDatabases(ApplicationConfig.MERGED_XML_FILE);
+                }
+                break;
+            } else {                                                      // No files selected
+                System.out.println(ApplicationConfig.FILE_SELECTION_ERROR);
+
+                if (returnValue == JFileChooser.CANCEL_OPTION) {          // Break out of the selection process
+                    break;
+                }
+            }
+
+        }
     }
 
     /**
      * Close all database resources
      */
     public void cleanup() {
-        System.out.print(ApplicationConfig.CLOSING_CONNECTIONS);                     // Notify user that connections are closing
+        System.out.print(ApplicationConfig.CLOSING_CONNECTIONS);          // Notify user that connections are closing
 
         MySQL.closeConnection();                                          // Close the database connection
         MySQL.closeStatement();                                           // Close the SQL statement object
         MySQL.closePreparedStatement();                                   // Close the PreparedStatement object
         MongoDB.closeConnection();                                                  // Close the MongoDB client
 
-        System.out.print(ApplicationConfig.CLOSED_CONNECTIONS);                      // Notify user that connections are closed
+        System.out.print(ApplicationConfig.CLOSED_CONNECTIONS);           // Notify user that connections are closed
     }
 
     /**
      * Notify the user that the ChatBot session has terminated
      */
     public void endMessage() {
-        System.out.print(ApplicationConfig.END_MESSAGE);                            // Terminate ChatBot message for the client
+        System.out.print(ApplicationConfig.END_MESSAGE);                  // Terminate ChatBot message for the client
     }
 
     //*****************************************************************************************************************
@@ -73,9 +97,14 @@ public class Builder {
     /*
      * Merge multiple XML documents into one
      */
-    private void mergeXML(ArrayList<String> selectedFiles) {
+    private void mergeXML(File[] files) {
 
-        FileMerger fileMerger = new FileMerger(selectedFiles, ApplicationConfig.MERGED_XML_FILE);
+        ArrayList<String> fileCollection = new ArrayList<>();
+        for (File file : files) {
+            fileCollection.add(file.getPath());
+        }
+
+        FileMerger fileMerger = new FileMerger(fileCollection, ApplicationConfig.MERGED_XML_FILE);
         try {
             fileMerger.merge();
         } catch (IOException e) {
@@ -86,12 +115,12 @@ public class Builder {
     /*
      * Build up the databases if they don't exist
      */
-    private void initDatabases() {
-        XMLParser parser = new XMLParser();
+    private void initDatabases(String filename) {
+        XMLParser parser = new XMLParser(filename);
 
         if (!LuceneIndex.exists()) {                            // If document hasn't been parsed then...
             parser.parse();                                     // Parse the entire document
-            parser.createLuceneIndex();                               // Build Lucene Index
+            parser.createLuceneIndex();                         // Build Lucene Index
         }
 
         if (!MySQL.exists()) {                                  // If database hasn't been built then...
